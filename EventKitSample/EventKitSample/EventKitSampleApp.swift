272c814755b9848715ddfe02fe9de2e63ad7a7d7
Gyeolee <gyeoleeeeeee@gmail.com>
2023-10-19 13:47:12 +0900

    [ADD] 프로젝트 생성

---
+++ EventKitSample/EventKitSample/EventKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventKitSampleApp.swift
+//  EventKitSample
+//
+//  Created by Hangyeol on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct EventKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
